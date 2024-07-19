1d51997d9f708c1eb41815b24e50d272469c63cb
nadiduno <nadiduno@gmail.com>
2024-07-18 23:15:39 -0300

    Chaves da criptografia

--- index.js
+++ index.js
@@ -4,7 +4,36 @@ function getValueAndEncrypt() {
     console.log(digitizedValue);
 }
   
-  function encrypt(digitizedValue) {
+function encrypt(digitizedValue) {
     const paragraphElement = document.querySelector('.MensagemEncrypt');
-    paragraphElement.textContent = digitizedValue;
+    const characterList = [];
+    for (let i = 0; i < digitizedValue.length; i++) {
+      
+      //Guardando os caracteres em minusculos em uma lista 
+      characterList.push(digitizedValue[i].toLowerCase()); 
+      
+      //Chaves da criptografia
+      switch (characterList[i]) {
+        case "a":
+          characterList[i] = "ai";
+          break;
+        case "e":
+          characterList[i] = "enter";
+          break;
+        case "i":
+          characterList[i] = "imes";
+          break;
+        case "o":
+          characterList[i] = "ober";
+          break;
+        case "u":
+          characterList[i] = "ufat";
+          break;
+        default:
+          characterList[i] = characterList[i];
+      }
+    }
+    //Unindo o array em uma cadeia de carateres
+    const encryptText = characterList.join("");
+    paragraphElement.textContent = encryptText;
   }
