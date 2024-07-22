3f8983cc80a0a0fa2476fb5c956c6ada516a8e41
nadiduno <nadiduno@gmail.com>
2024-07-21 23:54:08 -0300

    Limpando variaveis e tag html

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+var characterList = [];
+    
 function getValueAndEncrypt() {
-    var digitizedValue = document.getElementById("textInput").value;
-    encrypt(digitizedValue);
-    console.log(digitizedValue);
+  var digitizedValue = undefined  
+  digitizedValue = document.getElementById("textInput").value;
+  encrypt(digitizedValue);
+  console.log(digitizedValue);
 }
   
 function encrypt(digitizedValue) {
     const paragraphElement = document.querySelector('.MensagemEncrypt');
-    const characterList = [];
+    var characterList = [];
     for (let i = 0; i < digitizedValue.length; i++) {
       
       //Guardando os caracteres em minusculos em uma lista 
@@ -35,7 +38,9 @@ function encrypt(digitizedValue) {
     }
     //Unindo o array em uma cadeia de carateres novamnete
     const encryptText = characterList.join("");
+    paragraphElement.innerHTML = '';
     paragraphElement.textContent = encryptText;
+    console.log(characterList); 
     clipBoard(encryptText);
   }
 
@@ -53,4 +58,53 @@ function encrypt(digitizedValue) {
     .catch(error => {
         console.error("Erro ao copiar texto:", error);
     });
+  }
+
+function getValueAndDecrypt() {
+    var digitizedValue = undefined 
+    digitizedValue = document.getElementById("textInput").value;
+    decrypt(digitizedValue);
+    console.log(digitizedValue);
+}
+ 
+  function decrypt(digitizedValue){
+    const paragraphElement = document.querySelector('.MensagemEncrypt');
+    var characterList = [];
+    for (let i = 0; i < digitizedValue.length; i++) 
+      characterList.push(digitizedValue[i].toLowerCase());
+    let i =0;
+    console.log(characterList) 
+    while(i < characterList.length){
+      //Descriptografiando
+      switch (characterList[i]) {
+        case "a":
+          if(characterList[i]+characterList[i+1]=="ai")
+            characterList.splice(i+1,1);
+          break;
+        case "e":
+          if(characterList[i]+characterList[i+1]+characterList[i+2]+characterList[i+3]+characterList[i+4]=="enter")
+            characterList.splice(i+1,4);  
+          break;
+        case "i":
+          if(characterList[i]+characterList[i+1]+characterList[i+2]+characterList[i+3]=="imes")
+            characterList.splice(i+1,3);
+          break;
+        case "o":
+          if(characterList[i]+characterList[i+1]+characterList[i+2]+characterList[i+3]=="ober")
+            characterList.splice(i+1,3);
+          break;
+        case "u":
+          if(characterList[i]+characterList[i+1]+characterList[i+2]+characterList[i+3]=="ufat")
+            characterList.splice(i+1,3);
+          break;
+        default:
+          characterList[i] = characterList[i];
+      }
+      i++;
+    }
+    //Unindo o array em uma cadeia de carateres novamnete
+    const encryptText = characterList.join("");
+    paragraphElement.innerHTML = '';
+    paragraphElement.textContent = encryptText;
+    console.log(characterList); 
   }
